b18cdf9a9a9e3835491065b21de88a0a19180747
SEG <segdev.mx@gmail.com>
2024-04-07 21:30:20 -0600

    feat(config): add a hashmap and methods to edit and save channel ids

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,39 @@
-use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 
-#[derive(Clone, Copy, Debug, Default, Deserialize, Serialize)]
+use serde::{Deserialize, Serialize};
+use tracing::instrument;
+
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
 pub struct GuildConfig {
+    channels: HashMap<String, u64>,
+
     #[serde(skip_serializing, skip_deserializing)]
-    pub has_changed: bool,
+    has_changed: bool,
+}
+
+impl GuildConfig {
+    #[instrument(skip(self))]
+    pub fn has_changed(&self) -> bool {
+        self.has_changed
+    }
+
+    #[instrument(skip(self))]
+    pub fn get_channel(&self, key: &str) -> Option<u64> {
+        self.channels.get(key).copied()
+    }
+
+    #[instrument(skip(self))]
+    pub fn set_channel(&mut self, key: &str, channel_id: Option<u64>) {
+        if channel_id.is_none() {
+            self.channels.remove(key);
+            return;
+        }
+
+        self.channels.insert(
+            key.to_string(),
+            channel_id.expect("None case was handled earlier."),
+        );
+
+        self.has_changed = true;
+    }
 }
