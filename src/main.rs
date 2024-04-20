72e737659fc407cadea53ff98b47e53acbfd4329
SEG <segdev.mx@gmail.com>
2024-04-20 14:30:43 -0600

    feat: add an envfilter to use environment variables to configure the logs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use tracing_subscriber::EnvFilter;
+
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt().init();
+    let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+
+    tracing_subscriber::fmt().with_env_filter(filter).init();
 
     bloogger::init().await;
 }
